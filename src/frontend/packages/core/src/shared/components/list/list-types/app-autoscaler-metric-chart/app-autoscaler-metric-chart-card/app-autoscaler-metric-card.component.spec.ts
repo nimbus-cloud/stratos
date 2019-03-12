a323c5720bc9d2d755d47d73691921eec12513d1
Richard Cox <richard.cox@suse.com>
2019-03-12 11:27:08 +0000

    Fix test import

--- src/frontend/packages/core/src/shared/components/list/list-types/app-autoscaler-metric-chart/app-autoscaler-metric-chart-card/app-autoscaler-metric-card.component.spec.ts
+++ src/frontend/packages/core/src/shared/components/list/list-types/app-autoscaler-metric-chart/app-autoscaler-metric-chart-card/app-autoscaler-metric-card.component.spec.ts
@@ -1,18 +1,20 @@
 import { DatePipe } from '@angular/common';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {
-  ApplicationEnvVarsHelper,
-} from '../../../../../../features/applications/application/application-tabs-base/tabs/build-tab/application-env-vars.service';
 import { generateTestApplicationServiceProvider } from '../../../../../../../test-framework/application-service-helper';
 import { BaseTestModules } from '../../../../../../../test-framework/cloud-foundry-endpoint-service.helper';
 import { createBasicStoreModule } from '../../../../../../../test-framework/store-test-helper';
+import {
+  ApplicationEnvVarsHelper,
+} from '../../../../../../features/applications/application/application-tabs-base/tabs/build-tab/application-env-vars.service';
 import { ServiceActionHelperService } from '../../../../../data-services/service-action-helper.service';
 import { EntityMonitorFactory } from '../../../../../monitors/entity-monitor.factory.service';
 import { PaginationMonitorFactory } from '../../../../../monitors/pagination-monitor.factory';
 import { ApplicationStateService } from '../../../../application-state/application-state.service';
 import { ConfirmationDialogService } from '../../../../confirmation-dialog.service';
-import { AppServiceBindingCardComponent } from './app-service-binding-card.component';
+import {
+  AppServiceBindingCardComponent,
+} from '../../app-sevice-bindings/app-service-binding-card/app-service-binding-card.component';
 
 describe('AppServiceBindingCardComponent', () => {
   let component: AppServiceBindingCardComponent;
